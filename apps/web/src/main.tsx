373b06fad84b67215940ec55a7f66056e680101f
usrnmtkn <usrnmtkn@users.noreply.github.com>
2026-06-04 18:23:13 -0400

    Isolate admin route styles

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -1,10 +1,22 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
-import "./styles.css";
 
-createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function isAdminContentPath() {
+  return window.location.pathname === "/admin/content" || window.location.pathname === "/admin/generated-content";
+}
+
+async function startApp() {
+  if (!isAdminContentPath()) {
+    await import("./styles.css");
+  }
+
+  const { App } = await import("./App");
+
+  createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+void startApp();
