dd1828710626f183d5b1529f511dc8ce401a8878
usrnmtkn <usrnmtkn@proton.me>
2026-09-04 13:15:38 -0400

    Hide Content Studio row selection controls

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "../../web/src/styles/theme.css";
 import "../../web/src/styles/pill.css";
 import { GeneratedContentAdminDashboard } from "./GeneratedContentAdminDashboard";
+import "./admin-row-selection.css";
 
 createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
