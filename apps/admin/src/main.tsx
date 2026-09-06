749e54b98ba8e5006b209b0792bf4485d560d9ac
usrnmtkn <usrnmtkn@proton.me>
2026-09-06 11:43:37 -0400

    Redesign production Content Studio editor workflow

--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -6,7 +6,6 @@ import { GeneratedContentAdminDashboard } from "./GeneratedContentAdminDashboard
 import { setupAdminReaderLinkTargets } from "./adminReaderLinks";
 import "./admin-row-selection.css";
 import "./admin-form-density.css";
-import "./admin-content-studio-ux-compat.css";
 
 const ContentCoverageDashboard = lazy(() => import("./ContentCoverageDashboard"));
 
